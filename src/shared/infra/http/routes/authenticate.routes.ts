8382b101405f8cd62833167d8c8566ae8b1e059f
lucas <jfontinele9490@gmail.com>
2021-04-13 14:32:44 -0300

    finalizando o controller do refresh token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import AuthenticateUserController from "@modules/Accounts/useCases/authenticateUser/AuthenticateUserController";
+import RefreshTokenController from "@modules/Accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
-
+const refreshTokenController = new RefreshTokenController();
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
-
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 export { authenticateRoutes };
